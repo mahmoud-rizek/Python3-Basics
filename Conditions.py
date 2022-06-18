81e949673ff9a5c4964c7925f39e3c1f33f0d069
gamikim328 <mahmoudtino24@gmail.com>
2022-06-18 16:43:51 +0200

    add some changes for conditions

--- Conditions.py
+++ Conditions.py
@@ -2,10 +2,12 @@
 
 x = 100
 o = 565
+
 if x==100:
     print("Done")
 
-# if else condition
+# if ...else condition
+
 if o==100:
     print('Tasck is done')
 else:
@@ -13,6 +15,7 @@ else:
 
 
 # elif condition
+
 username = 'khaled'
 
 if username == 'mahmoud' :
@@ -23,4 +26,48 @@ elif username == 'mohsen':
     print('welcome mohsen')
 else:
     print("you aren't our friends")
+
+
+# Nested if
+
+w = 333
+
+if w < 500:
+    print('w < 500')
+
+    if w < 357:
+        print('w < 357')
+    else:
+        print('w > 357')
+else:
+    print('w > 500')
+
+
+
+# Short hand if
+i = 19
+if i > 15: print('i > 15')
+
+# Short Hand If ... Else
+
+name = 'mohamed'
+print('Hola mahmoud') if name == 'mahmoud' else print("you aren't mahmuod")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
