93939defc6638abdefb37cdd751a6419793c4f7f
gamikim328 <mahmoudtino24@gmail.com>
2022-06-19 05:20:34 +0200

    Program transform the age by seconds

---
+++ transform you age by seconds.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age:"))
+age_months = age * 12
+age_days = age * 365
+age_hours = age_days * 24
+age_mintes = age_hours * 60
+age_seconds = age_mintes * 60
+print(f"Your age is {age_months} month and {age_days} days and {age_hours} hours and {age_mintes} mintes and {age_seconds} seconds")
