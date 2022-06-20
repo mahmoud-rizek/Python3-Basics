a652e9b01ff1426c342086ad10d62759653aa4b7
gamikim328 <mahmoudtino24@gmail.com>
2022-06-20 04:35:22 +0200

    add loop-files

---
+++ 02 While-loop.py
@@ -0,0 +1,24 @@
+myfriends = ["ali", "ezz" , "zimaa" , "abdelhamid" , "ahmed" , "abdalla" , "adhame" , "ibrahim" , "mostafa" , "mahmoud" , "walid" , "naser"]
+
+a = 0
+
+while a < len(myfriends):
+    print(f"#{str(a + 1).zfill(2)} {myfriends[a]}")
+    a += 1
+    
+
+# print(len(myfriends))
+'''
+print(myfriends[0])
+print(myfriends[1])
+print(myfriends[2])
+print(myfriends[3])
+print(myfriends[4])
+print(myfriends[5])
+print(myfriends[6])
+print(myfriends[7])
+print(myfriends[8])
+print(myfriends[9])
+print(myfriends[10])
+print(myfriends[11])
+'''
